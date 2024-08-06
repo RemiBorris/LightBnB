f2c49978800a98b59d2c15452f44ee8daffa2563
remiborris <remiborris@gmail.com>
2024-08-06 13:39:44 -0300

    recreate addproperty function to add a property with a sql query

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -130,9 +130,9 @@ const getAllReservations = function (guest_id, limit = 10) {
 // };
 
 const getAllProperties = (options, limit = 10) => {
-  // 1
+
   const queryParams = [];
-  // 2
+
   let queryString = `
   SELECT properties.*, avg(property_reviews.rating) as average_rating
   FROM properties
@@ -140,7 +140,6 @@ const getAllProperties = (options, limit = 10) => {
   WHERE 1=1
   `;
 
-  // 3
   if (options.city) {
     queryParams.push(`%${options.city}%`);
     queryString += `AND city LIKE $${queryParams.length} `;
@@ -157,8 +156,7 @@ const getAllProperties = (options, limit = 10) => {
     queryParams.push(`${options.maximum_price_per_night * 100}`);
     queryString += `AND cost_per_night <= $${queryParams.length}`;
   }
-   
-  // 4
+
   queryString += `
   GROUP BY properties.id
   `;
@@ -174,10 +172,7 @@ const getAllProperties = (options, limit = 10) => {
   LIMIT $${queryParams.length};
   `;
 
-  // 5
-  console.log(queryString, queryParams);
 
-  // 6
   return pool
     .query(queryString, queryParams)
     .then((res) => res.rows)
@@ -192,10 +187,33 @@ const getAllProperties = (options, limit = 10) => {
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function (property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  return pool
+    .query(`
+      INSERT INTO properties (
+      owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, province, city, country, street, post_code) 
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+      RETURNING *;`,
+    [ property.owner_id,
+      property.title,
+      property.description,
+      property.thumbnail_photo_url,
+      property.cover_photo_url,
+      property.cost_per_night,
+      property.parking_spaces,
+      property.number_of_bathrooms,
+      property.number_of_bedrooms,
+      property.province,
+      property.city,
+      property.country,
+      property.street,
+      property.post_code,
+    ])
+    .then(res => {
+      return res.rows[0];
+    })
+    .catch(err => {
+      console.log(err.message);
+    });
 };
 
 module.exports = {
